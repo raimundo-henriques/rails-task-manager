32b5b9a3e828627426e5fc4ac44cdd27ee968c8b
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-17 17:22:53 +0100

    Done but not pretty

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,17 +17,23 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
   # 3 - As a user, I can list tasks
-  get 'tasks', to: 'tasks#index'
-  # 5 - As a user, I can add a new task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  # 4 - As a user, I can view the details of a task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # 6 - As a user, I can edit a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+  # get 'tasks', to: 'tasks#index'
+  # # 5 - As a user, I can add a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # # 4 - As a user, I can view the details of a task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # 6 - As a user, I can edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update', as: :update_task
   # 7 - As a user, I can remove a task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
