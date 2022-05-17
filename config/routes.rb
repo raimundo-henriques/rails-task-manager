5e5d0e5ab11aac19e7b62f3c6ce1f0048a5f9252
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-05-17 13:02:07 +0100

    Ex 3 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # As a user, I can list tasks
+  get 'tasks', to: 'tasks#tasks'
 end
